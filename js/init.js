3ed6a73c336d1530eb6ee7c101fb9d2cb15b0b1a
cutme <info@cutme.pl>
2016-02-12 00:13:21 +0100

    Optimize

--- js/init.js
+++ js/init.js
@@ -3,10 +3,7 @@ head.js('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js')
 	.js('js/owl.carousel.min.js')
 	.js('https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.1/jquery.magnific-popup.min.js')
 	.load('https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.1/magnific-popup.min.css')
-	.js('https://cdnjs.cloudflare.com/ajax/libs/jquery.transit/0.9.12/jquery.transit.js')
-	.js('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js')
-	.js('https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.3.1/mobile-detect.js')
-	.js('https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.3.1/mobile-detect-modernizr.js')
+	.js('https://cdnjs.cloudflare.com/ajax/libs/jquery.transit/0.9.12/jquery.transit.min.js')
 	.js('js/main.js');
 
 if (head.browser.ie && parseFloat(head.browser.version) < 9) {
